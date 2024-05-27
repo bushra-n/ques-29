30af3715e219347fa088fe23ffd784f661fc557f
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-27 09:21:13 +0500

    Add files via upload

---
+++ task29.ts
@@ -0,0 +1,40 @@
+// Task 29
+let favourite_fruit:string[] = ["mango","cherry","strawberry","Plum","orange"]
+if (`${favourite_fruit[0]}` === "mango"){
+    console.log ("I really like mango")
+}
+else {
+    console.log("I don't like this fruit")
+}
+if (`${favourite_fruit[1]}` === "cherry"){
+    console.log ("I really like cherry")
+}
+else{
+    console.log("I don't like this fruit")
+}
+if (`${favourite_fruit[2]}` === "strawberry"){
+    console.log ("I really like strawberry")
+}
+else{
+    console.log("I don't like this fruit")
+}
+if (`${favourite_fruit[3]}` === "papaya"){
+    console.log ("I really like papaya")
+}
+else{
+    console.log("I don't like this fruit")
+}
+if (`${favourite_fruit[4]}` === "peach"){
+    console.log ("I really like peach")
+}
+else{
+    console.log("I don't like this fruit")
+}
+
+
+
+
+
+
+
+
